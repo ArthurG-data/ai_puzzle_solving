395a37e3ed39db1b363efdd5d2b932ee180f4e27
Arthur Guillaume <71298154+ArthurG-data@users.noreply.github.com>
2025-01-18 20:53:58 +1000

    Update mySokobanSolver.py

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -1,38 +1,15 @@
-'''
-IFN680 Sokoban Assignment
-
-The functions and classes defined in this module will be called by a marker script. 
-You should complete the functions and classes according to their specified interfaces.
-
-You are not allowed to change the defined interfaces.
-That is, changing the formal parameters of a function will break the 
-interface and triggers to a fail for the test of your code.
-'''
-
 import search
 import sokoban
 import numpy as np
 import time
 from search import Node
 
-def my_team():
-    '''
-    Return the list of the team members of this assignment submission as a list
-    of triplet of the form (student_number, first_name, last_name)
-    e.g.  [ (1234567, 'Ada', 'Lovelace'), (1234568, 'Grace', 'Hopper'), (1234569, 'Eva', 'Tardos') ]
-    '''
-
-    return [(11393611, 'Yu-Ying', 'Tang'), (11371200, 'Arthur', 'Guillaume')]
- 
 
 def taboo_cells(warehouse):
     '''  
-    Identify the taboo cells of a warehouse. A cell inside a warehouse is 
+    Identifies the taboo cells of a warehouse. A cell inside a warehouse is 
     called 'taboo' if whenever a box get pushed on such a cell then the puzzle 
     becomes unsolvable.  
-    When determining the taboo cells, you must ignore all the existing boxes, 
-    simply consider the walls and the target cells.  
-    Use only the following two rules to determine the taboo cells;
      Rule 1: if a cell is a corner inside the warehouse and not a target, 
              then it is a taboo cell.
      Rule 2: all the cells between two corners inside the warehouse along a 
@@ -43,10 +20,7 @@ def taboo_cells(warehouse):
     @return
        A string representing the puzzle with only the wall cells marked with 
        an '#' and the taboo cells marked with an 'X'.  
-       The returned string should NOT have marks for the worker, the targets,
-       and the boxes.  
     '''
-    ##         "INSERT YOUR CODE HERE"  
 
     # retrieving warehouse
     warehouse = str(warehouse).split('\n')
@@ -149,26 +123,14 @@ class SokobanPuzzle(search.Problem):
     An instance contains information about the walls, the targets, the boxes
     and the worker.
 
-    Your implementation should be fully compatible with the search functions of 
-    the provided module 'search.py'. It uses search.Problem as a sub-class. 
-    That means, it should have a:
-    - self.actions() function
-    - self.result() function
-    - self.goal_test() function
-    See the Problem class in search.py for more details on these functions.
-    
-    Each instance should have at least the following attributes:
-    - self.allow_taboo_push
-    - self.macro
-    
-    When self.allow_taboo_push is set to True, the 'actions' function should 
-    return all possible legal moves including those that move a box on a taboo 
-    cell. If self.allow_taboo_push is set to False, those moves should not be
+    When self.allow_taboo_push is set to True, the 'actions' function
+    returns all possible legal moves including those that move a box on a taboo 
+    cell. If self.allow_taboo_push is set to False, those moves are not
     included in the returned list of actions.
     
-    If self.macro is set True, the 'actions' function should return 
-    macro actions. If self.macro is set False, the 'actions' function should 
-    return elementary actions.
+    If self.macro is set True, the 'actions' function returns 
+    macro actions. If self.macro is set False, the 'actions' function
+    returns elementary actions.
     '''
     
     def __init__(self, warehouse, goal=None, allow_taboo_push=False, macro=False, allow_push=True):
@@ -384,10 +346,9 @@ class SokobanPuzzle(search.Problem):
 def check_action_seq(warehouse, action_seq):
     '''
     Determine if the sequence of actions listed in 'action_seq' is legal or not.
-    
-    Important notes:
-      - a legal sequence of actions does not necessarily solve the puzzle.
-      - an action is legal even if it pushes a box onto a taboo cell.
+
+  - a legal sequence of actions does not necessarily solve the puzzle.
+  - an action is legal even if it pushes a box onto a taboo cell.
         
     @param warehouse: a valid Warehouse object
 
@@ -400,11 +361,9 @@ def check_action_seq(warehouse, action_seq):
                         or push one box into a wall, or walk into a wall.
         Otherwise, if all actions were successful, return                 
                A string representing the state of the puzzle after applying
-               the sequence of actions.  This must be the same string as the
-               string returned by the method  Warehouse.__str__()
+               the sequence of actions. 
     '''
-    ##         "INSERT YOUR CODE HERE"
-
+ 
     # retrieve states from the warehouse
     walls = set(warehouse.walls)
     targets = set(warehouse.targets)
@@ -474,7 +433,7 @@ def check_action_seq(warehouse, action_seq):
 
 def solve_sokoban_elem(warehouse):
     '''    
-    This function should solve using elementary actions 
+    This function should solves elementary actions 
     the puzzle defined in a file.
     
     @param warehouse: a valid Warehouse object
@@ -486,8 +445,6 @@ def solve_sokoban_elem(warehouse):
             For example, ['Left', 'Down', Down','Right', 'Up', 'Down']
             If the puzzle is already in a goal state, simply return []
     '''
-
-    ##         "INSERT YOUR CODE HERE"
     start_time = time.time()
     timeout = 180 # 3 mins
     
@@ -530,8 +487,7 @@ def can_go_there(warehouse, dst):
       True if the worker can walk to cell dst=(row,column) without pushing any box
       False otherwise
     '''
-   
-    ##         "INSERT YOUR CODE HERE"
+
     goal = (dst[1], dst[0])
 
     sokoban = SokobanPuzzle(warehouse, goal=goal, allow_push=False) 
@@ -543,7 +499,7 @@ def can_go_there(warehouse, dst):
 def solve_sokoban_macro(warehouse):
     '''    
     Solve using macro actions the puzzle defined in the warehouse passed as
-    a parameter. A sequence of macro actions should be 
+    a parameter. A sequence of macro actions is 
     represented by a list M of the form
             [ ((r1,c1), a1), ((r2,c2), a2), ..., ((rn,cn), an) ]
     For example M = [ ((3,4),'Left') , ((5,2),'Up'), ((12,4),'Down') ] 
@@ -558,7 +514,7 @@ def solve_sokoban_macro(warehouse):
         Otherwise return M a sequence of macro actions that solves the puzzle.
         If the puzzle is already in a goal state, simply return []
     '''
-    ##         "INSERT YOUR CODE HERE"
+  
     start_time = time.time()
     timeout = 180 # 3 mins
     
